a46d44300ed02b02d9ff60744d07e92ecbeaa316
alexandre <amenezes031@gmail.com>
2020-07-13 16:36:12 -0300

    Tipos avançados - tupla

---
+++ src/aula_04/aula04.js
@@ -0,0 +1,8 @@
+//Tipos avançados
+//Tuplas - conjunto de dados relacionados
+var campeaoBrasilerio = ["Palmeiras", 2016];
+alert(campeaoBrasilerio[0] + ", " + campeaoBrasilerio[1]);
+//Permite alteração de valor
+campeaoBrasilerio[0] = "Teste";
+alert(campeaoBrasilerio[0] + ", " + campeaoBrasilerio[1]);
+//Enums
---
+++ src/aula_04/aula04.ts
@@ -0,0 +1,10 @@
+//Tipos avançados
+//Tuplas - conjunto de dados relacionados
+let campeaoBrasilerio: [string, number] = ["Palmeiras", 2016];
+alert(campeaoBrasilerio[0] + ", " + campeaoBrasilerio[1]);
+
+//Permite alteração de valor
+campeaoBrasilerio[0] = "Teste";
+alert(campeaoBrasilerio[0] + ", " + campeaoBrasilerio[1]);
+
+//Enums
