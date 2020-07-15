703f5c11ed977c7e3ce071d60037d1df13114163
alexandre <amenezes031@gmail.com>
2020-07-15 13:30:11 -0300

    Tipo de dados enum, any e object

--- src/aula_04/aula04.js
+++ src/aula_04/aula04.js
@@ -6,3 +6,23 @@ alert(campeaoBrasilerio[0] + ", " + campeaoBrasilerio[1]);
 campeaoBrasilerio[0] = "Teste";
 alert(campeaoBrasilerio[0] + ", " + campeaoBrasilerio[1]);
 //Enums
+var Estado;
+(function (Estado) {
+    Estado[Estado["Desabilitado"] = 0] = "Desabilitado";
+    Estado[Estado["Habilitado"] = 1] = "Habilitado";
+})(Estado || (Estado = {}));
+;
+var meuEstado = Estado.Desabilitado;
+alert(meuEstado);
+var textoMeuEstado = Estado[Estado.Desabilitado];
+alert(textoMeuEstado);
+//Any - ao utilizar o tipo any, o TypeScript deixa de fazer checagem de tipo.
+var v = "Teste";
+alert(v);
+v = 2;
+alert(v);
+alert(v.toExponential());
+alert(v.toMetodo()); // o TypeScript não verifica se o método existe e gera erro somente quando executado
+//Object - tudo é objeto no JavaScript e no TypeScript
+var v2 = 3;
+alert(v2.toExponential());
--- src/aula_04/aula04.ts
+++ src/aula_04/aula04.ts
@@ -8,3 +8,22 @@ campeaoBrasilerio[0] = "Teste";
 alert(campeaoBrasilerio[0] + ", " + campeaoBrasilerio[1]);
 
 //Enums
+enum Estado { Desabilitado, Habilitado };
+const meuEstado: Estado = Estado.Desabilitado;
+alert(meuEstado);
+
+const textoMeuEstado: string = Estado[Estado.Desabilitado];
+alert(textoMeuEstado);
+
+//Any - ao utilizar o tipo any, o TypeScript deixa de fazer checagem de tipo.
+let v: any = "Teste";
+alert(v);
+
+v = 2;
+alert(v);
+alert(v.toExponential());
+alert(v.toMetodo()); // o TypeScript não verifica se o método existe e gera erro somente quando executado
+
+//Object - tudo é objeto no JavaScript e no TypeScript
+const v2: Object = 3;
+alert(v2.toExponential());
